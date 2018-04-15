8c0bcb467ac90b1e8ba071bcb8a4531dae056712
Sunny GUAN <guanxingbo@126.com>
2018-04-15 19:34:46 +0800

    first

---
+++ bar/src/main/java/org/ros/android/rviz_for_android/FloatMath.java
@@ -0,0 +1,14 @@
+package org.ros.android.rviz_for_android;
+
+public class FloatMath {
+    private FloatMath(){};
+    public static Float sqrt(Float op){
+        return (float)Math.sqrt(op);
+    }
+    public static Float cos(Float op){
+        return (float)Math.cos(op);
+    }
+    public static Float sin(Float op){
+        return (float)Math.sin(op);
+    }
+}
